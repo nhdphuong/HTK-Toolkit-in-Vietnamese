f84d468f6f405c03635d7ccd9848f13ac3997408
Phuong H.D Nguyen <Phuong Nguyen>
2018-06-18 03:12:23 +0700

    changing to use x64 HTK binary

--- discriminativeTrain.sh
+++ discriminativeTrain.sh
@@ -7,14 +7,14 @@ set -v
 
 # 2. Building Language Model
 # Database preparation
-htk/LNewMap -f WFC LMName gram/empty.wmap
-htk/LGPrep -a 100000 -b 200000 -n 3 -d gram gram/empty.wmap train/collection.txt
+htk/x64/LNewMap -f WFC LMName gram/empty.wmap
+htk/x64/LGPrep -a 100000 -b 200000 -n 3 -d gram gram/empty.wmap train/collection.txt
 
 # Language model generation
-htk/LBuild -c 2 0 -c 3 0 gram/wmap gram/langmodel gram/gram.*
+htk/x64/LBuild -c 2 0 -c 3 0 gram/wmap gram/langmodel gram/gram.*
 
 # Testing the LM perplexity
-htk/LPlex -t gram/langmodel test/collection.txt
+htk/x64/LPlex -t gram/langmodel test/collection.txt
 
 # 3. Recognizing
 # Preparing Data
@@ -25,10 +25,10 @@ perl script/prompts2mlf.pl mlf/testLM_words.mlf txt/testLM_prompts.txt
 python script/listwavmfc.py c:/Active/WorkingProjects/anotherHTK/test1/ txt/testLM.scp txt/testLM_mfc.scp
 
 # Recognizing
-htk/HDecode.orig -H htmm/hmm15/macros -H htmm/hmm15/hmmdefs -S txt/testLM_mfc.scp -t 220.0 220.0 -C cfg/config.hdecode -i mlf/recoutLM.mlf -w gram/langmodel -p 0.0 -s 5.0 txt/dict.txt phones/tiedlist
+htk/x64/HDecode.orig -H htmm/hmm15/macros -H htmm/hmm15/hmmdefs -S txt/testLM_mfc.scp -t 220.0 220.0 -C cfg/config.hdecode -i mlf/recoutLM.mlf -w gram/langmodel -p 0.0 -s 5.0 txt/dict.txt phones/tiedlist
 
 # Format Results
-htk/HResults -I mlf/test_words.mlf phones/tiedlist mlf/recout.mlf | tee resultLM.txt
+htk/x64/HResults -I mlf/test_words.mlf phones/tiedlist mlf/recout.mlf | tee resultLM.txt
 
 echo "Training completed"
 read
