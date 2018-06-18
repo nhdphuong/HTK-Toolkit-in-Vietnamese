8c43adba20539410da0d8af6046e9a083655a821
Phuong H.D Nguyen <7239454+nhdphuong@users.noreply.github.com>
2018-06-19 00:30:07 +0700

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -4,7 +4,7 @@ set -v
 # Recognizer Evaluation
 # Step 11 - Recognizing the Test Data
 # Evaluation prompts - number only if no prompts
-# python script/generatePrompts.py datasets/eval/waves/ datasets/eval/prompts.txt
+python script/generatePrompts.py datasets/eval/waves/ datasets/eval/prompts.txt
 python script/vietnameseToTelex.py datasets/eval/prompts.txt txt/eval_prompts.txt
 perl script/prompts2mlf.pl mlf/eval_words.mlf txt/eval_prompts.txt
 
