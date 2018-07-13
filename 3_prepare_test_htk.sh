7e6d193c9d35a751dea4ce43c3f25ea7b397bf3a
Phuong Nguyen <nhdphuong93@gmail.com>
2018-07-13 17:23:57 +0700

    update test file

--- 3_prepare_test_htk.sh
+++ 3_prepare_test_htk.sh
@@ -14,9 +14,4 @@ python script/listwavmfc.py datasets/eval/waves/ txt/eval.scp txt/eval_mfc.scp
 # Evaluation MLF
 htk/HCopy -C cfg/HCopy.cfg -S txt/eval.scp
 
-# Recognizing the Test Data
-htk/HVite -C cfg/HVite.cfg -H hmm/hmm15/macros -H hmm/hmm15/hmmdefs -S txt/eval_mfc.scp -i mlf/eval_recout.mlf -w txt/wdnet.txt txt/srcDict.txt phones/tiedlist
-
-# Format results
-htk/HResults -I mlf/eval_words.mlf phones/tiedlist mlf/eval_recout.mlf | tee result_eval.txt
 
