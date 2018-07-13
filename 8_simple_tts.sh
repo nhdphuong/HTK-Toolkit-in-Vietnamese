2d1eae6591a81d467428a279303021d1eb97889b
Phuong Nguyen <nhdphuong93@gmail.com>
2018-07-14 01:38:06 +0700

    update a little tts script

--- 8_simple_tts.sh
+++ 8_simple_tts.sh
@@ -5,7 +5,7 @@ set -xv
 python script/listwavmfc.py datasets/train/waves/VIVOSSPK14/ txt/tts.scp txt/tts_mfc.scp
 
 # Generating Forced Alignments
-htk/HVite34 -l '*' -a -b silence -m -I mlf/tts_words.mlf -H hmm/hmm15/macros -H hmm/hmm15/hmmdefs -i mlf/force_aligned.out txt/srcDict.txt phones/tiedlist -S txt/tts_mfc.scp
+htk/HVite34 -l '*' -a -b silence -m -I mlf/words.mlf -H hmm/hmm15/macros -H hmm/hmm15/hmmdefs -i mlf/force_aligned.out txt/srcDict.txt phones/tiedlist -S txt/tts_mfc.scp
 
 # Generating Speech from text
 python script/createContentWavFiles.py mlf/force_aligned.out datasets/test_number.txt datasets/train/waves/VIVOSSPK14 result_tts.wav txt/tts_content.txt
